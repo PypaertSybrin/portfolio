c7de6eb8fb3dd0460d82f4c0708e0bcafbf637f9
PypaertSybrin <sybrin.pypaert@student.howest.be>
2024-04-08 10:23:55 +0200

    feat: navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/generic/Header";
+import Navigation from "@/components/generic/Navigation";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-red-400 font-soleil">Testing</p>
+    <div className="min-h-screen dark:bg-neutral-900 dark:text-neutral-50 bg-neutral-50 text-neutral-900">
+      <Navigation />
+      <main>
+        <Header />
+      </main>
+    </div>
   );
 }
---
+++ src/components/generic/Navigation.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+const Navigation = () => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 p-4">
+      <div className="mx-auto max-w-screen-xl">
+        <ul className="text-2xl font-semibold flex gap-8 justify-end">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="#skills">Skills</Link>
+          </li>
+          <li>
+            <Link href="#projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="#about">About me</Link>
+          </li>
+          <li>
+            <Link href="#contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navigation;
